929f78ae7947308267902f18be89789e4a4f814b
Timoté LANCELLE <timote.lancelle@outlook.fr>
2022-01-26 03:29:15 +0100

    Correction controllers like, ajout BDD clean à jour

--- database/groupomania_network_dev.sql
+++ database/groupomania_network_dev.sql
@@ -47,31 +47,33 @@ LOCK TABLES `comments` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `likes_posts`
+-- Table structure for table `likes`
 --
 
-DROP TABLE IF EXISTS `likes_posts`;
+DROP TABLE IF EXISTS `likes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `likes_posts` (
+CREATE TABLE `likes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `userId` int NOT NULL,
   `postId` int NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   KEY `postId` (`postId`),
-  CONSTRAINT `likes_posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `likes_posts_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `likes_posts`
+-- Dumping data for table `likes`
 --
 
-LOCK TABLES `likes_posts` WRITE;
-/*!40000 ALTER TABLE `likes_posts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `likes_posts` ENABLE KEYS */;
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -144,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-26  0:28:34
+-- Dump completed on 2022-01-26  3:28:12
